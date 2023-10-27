c0b2c20e5dd03df18c65190522c0d0ba21435c65
Anderson Muniz <anderson.muniz8@gmail.com>
2023-10-27 17:19:04 -0400

    Add project files.

---
+++ APICatalogo/Migrations/20231026002838_PopulaCategorias.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Value('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
---
+++ APICatalogo/Migrations/20231026003640_test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+          
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+           
+        }
+    }
+}
